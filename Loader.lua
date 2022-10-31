5394e988aef50595e2cf081bc79b391cc0c0e188
xshad0xx <114771213+xshad0xx@users.noreply.github.com>
2022-10-31 14:09:08 +0100

    Add files via upload

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,7 @@
+local Inviter = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Discord%20Inviter/Source.lua"))()
+
+Inviter.Join("https://dsc.gg/bruhmasu")
+
 local games = {
     [{155615604}] = "https://raw.githubusercontent.com/xshad0xx/Chroma-Hub/main/PrisonLife/Script.lua",
     [{7187504655}] = "https://raw.githubusercontent.com/xshad0xx/Chroma-Hub/main/RockSimulator/Script.lua"
