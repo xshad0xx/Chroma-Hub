43e23f6d94469f27b6964b2c74ef3f2cd0d0e33d
xshad0xx <114771213+xshad0xx@users.noreply.github.com>
2022-11-03 21:30:03 +0100

    Add files via upload

---
+++ TsunamiGame/Loader.lua
@@ -0,0 +1,23 @@
+setclipboard("https://dsc.gg/bruhmasu")
+
+local count = 0
+
+repeat
+    warn("Check Out The Chroma Hub Discord Server! The Link Has Been Copied To Your Clipboard!")
+    count = count + 1
+until count == 3
+
+local games = {
+    [{155615604}] = "https://raw.githubusercontent.com/xshad0xx/Chroma-Hub/main/PrisonLife/Script.lua",
+    [{7187504655}] = "https://raw.githubusercontent.com/xshad0xx/Chroma-Hub/main/RockSimulator/Script.lua",
+    [{4893679160}] = "https://raw.githubusercontent.com/xshad0xx/Chroma-Hub/main/BigBrainSim/Script.lua",
+    [{7993293100}] = "https://raw.githubusercontent.com/xshad0xx/Chroma-Hub/main/TsunamiGame/Script.lua"
+}
+
+for ids, url in next, games do
+    if table.find(ids, game.PlaceId) then
+        loadstring(game:HttpGet(url))(); break
+    end
+end
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshad0xx/Chroma-Hub/main/Universal/Script.lua"))()
