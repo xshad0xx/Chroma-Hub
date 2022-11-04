9bd09ce764503c66ea409f756534183cde0767df
xshad0xx <114771213+xshad0xx@users.noreply.github.com>
2022-11-04 15:15:14 +0100

    Add files via upload

--- Universal/Script.lua
+++ Universal/Script.lua
@@ -1,1 +1,9 @@
+game.StarterGui:SetCore("SendNotification",{
+    Title = "Unsupported Game",
+    Text = "This Game Isn't Supported! Loading Universal Script...",
+    Duration = 10,
+    Button1 = "OK"
+})
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/ic3w0lf22/Unnamed-ESP/master/UnnamedESP.lua',true))()
